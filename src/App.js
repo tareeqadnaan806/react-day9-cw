7cc94b13c00570f75b8986d493e46837e55e79b8
tareeqadnaan806 <tareeqadnaan806@gmail.com>
2023-11-10 23:54:38 +0530

    sdfs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [uppercase, setUppercase] = useState(false);
+  const [lowercase, setLowercase] = useState(false);
+  const [symbol, setSymbol] = useState(false);
+  const [number, setNumber] = useState(false);
+  const [length, setLength] = useState(8);
+  const [password, setPassword] = useState("");
+
+  const upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  const lowerCase = "qwertyuiopasdfghjklzxcvbnm";
+  const symbols = "!@#$%^&*()_+/*-+?><.,~`";
+  const numbers = "0123456789";
+
+  let generatedPassword = upperCase;
+  if (uppercase === true) {
+    generatedPassword += upperCase;
+  }
+  if (lowercase === true) {
+    generatedPassword += lowerCase;
+  }
+  if (symbol === true) {
+    generatedPassword += symbols;
+  }
+  if (number === true) {
+    generatedPassword += numbers;
+  }
+
+  const pswdClick = () => {
+    var pwd = "";
+    for (let i = 0; i < length; i++) {
+      console.log(generatedPassword);
+      const password = Math.floor(Math.random() * generatedPassword.length);
+      console.log(password);
+      const generatedPswd = generatedPassword[password];
+      console.log(generatedPswd);
+      pwd = pwd + generatedPswd;
+    }
+    setPassword(pwd);
+  };
+
+  function handleLowercase() {
+    setLowercase(!lowercase);
+  }
+  function handleNumbers() {
+    setNumber(!number);
+  }
+  function handleSymbols() {
+    setSymbol(!symbol);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="text-xl font-bold m-3">Random Password Generator</h1>
+      <div>
+        <label htmlFor="length">Enter Password Length: </label>
+        <input
+          type="number"
+          id="length"
+          value={length}
+          className="border-2 rounded p-0.5"
+          onChange={(e) => setLength(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="lowercase">Include Lowercase </label>
+        <input
+          type="checkbox"
+          id="lowercase"
+          value={lowercase}
+          onChange={handleLowercase}
+        />
+      </div>
+      <div>
+        <label htmlFor="symbols">Include Symbols </label>
+        <input
+          type="checkbox"
+          id="symbols"
+          value={symbol}
+          onChange={handleSymbols}
+        />
+      </div>
+      <div>
+        <label htmlFor="Numbers">Include Numbers </label>
+        <input
+          type="checkbox"
+          id="Numbers"
+          value={number}
+          onChange={handleNumbers}
+        />
+      </div>
+      <button
+        className="bg-slate-500 py-2 px-3 m-3 rounded text-white"
+        onClick={pswdClick}
+      >
+        Generate Password
+      </button>
+      <div>
+        <input
+          type="text"
+          className="rounded border-2 m-2 p-0.5"
+          value={password}
+        />
+      </div>
     </div>
   );
 }
